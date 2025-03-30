3fe1b14428b42f371d336beb4283b2c21d0bbf38
Jiali Wang <jiali.wang@stud.h-da.de>
2025-03-30 18:05:38 +0200

    优化了拖动，新增聊天功能

--- content.js
+++ content.js
@@ -1,65 +1,103 @@
 let isDragging = false;
-let img
-function initDrag(){
-    console.log('create');
-    img=createImageElement();
-    img.addEventListener('mousedown',startDragging);
-    document.addEventListener('mousemove',handleDragging);
+let container;
+//TODO 无法拖拽bug
+const chat=[
+    "所有，或一无所有。",
+    "朋友，用太久设备可不好，记得休息哦。",
+    "有点无聊呢，要不要和我...",
+    "筹码掉了，哈哈，我也有失手的时候。其实是光顾着看朋友，分心啦。",
+    "又是头奖，理所应当的结果。",
+    "如果朋友抽到头奖，会做什么？现在，有一个实现的机会。",
+    "今天适宜买彩票，我打包票！",
+    "不要总是看页面，看看我嘛。",
+    "喵~",
+    "朋友喜欢吃什么？我去订餐厅。",
+    "匹诺康尼有家不错的店，想带朋友去，也许朋友不感兴趣...算啦。",
+    "给朋友买了礼物，猜猜看，是什么~",
+    "我是不是说了太多，打扰到朋友了？抱歉，因为一见到你，就忍不住分享最近的事。"
+];
 
-    img.addEventListener('mouseup',stopDragging);
+function init(){
+    createElement();
+    initDrag();
+    talk();
+};
+function createElement(){
+    //create container
+    container=document.createElement('div');
+    container.className='normalCon';
+    //create pic
+    const pic=document.createElement('img');
+    pic.className='normalPic';
+    pic.src=chrome.runtime.getURL('data/3.png');
+    container.appendChild(pic);
+    //create dialog
+    const dialog=document.createElement('div');
+    dialog.className='normalDialog';
+    dialog.style.display='none';
+    container.appendChild(dialog);
+
+    document.body.appendChild(container);
+};
+//拖拽开始
+function initDrag(){
+    console.log("initdrag"+container);
+    container.addEventListener('mousedown',startDragging);
+    document.addEventListener('mousemove',handleDragging);
+    container.addEventListener('mouseup',stopDragging);
     document.addEventListener('mouseup',stopDragging);
     document.addEventListener('mouseleave',stopDragging);
     document.addEventListener('blur',stopDragging);
-}
-
-function createImageElement(){
-    console.log('pic');
-    const element=document.createElement('img');
-    element.className='normalPic';
-    element.src=chrome.runtime.getURL('data/3.png');
-    document.body.appendChild(element);
-    console.log('图片地址:', element.src);
-    return element
-}
-
+    console.log("initdragend"+container);
+};
 function startDragging(e){
+    console.log("start");
     isDragging=true;
     e.preventDefault();
-    const rect=img.getBoundingClientRect();
+    const rect=container.getBoundingClientRect();
     const currentX=rect.left;
     const currentY=rect.top;
     const offsetX=e.clientX-currentX;
     const offsetY=e.clientY-currentY;
 
-    img.dataset.offsetX=offsetX;
-    img.dataset.offsetY=offsetY;
-}
-
+    container.dataset.offsetX=offsetX;
+    container.dataset.offsetY=offsetY;
+};
 function handleDragging(e){
     if (!isDragging) return;
-    const newX=e.clientX-parseFloat(img.dataset.offsetX);
-    const newY=e.clientY-parseFloat(img.dataset.offsetY);
+    console.log("handle");
+    const newX=e.clientX-parseFloat(container.dataset.offsetX);
+    const newY=e.clientY-parseFloat(container.dataset.offsetY);
 
     const [clampedX,clampedY]=applyBoundaryConstraints(newX,newY);
 
-    img.style.left=clampedX+'px';
-    img.style.top=clampedY+'px';
-}
-
+    container.style.left=clampedX+'px';
+    container.style.top=clampedY+'px';
+};
 function stopDragging(){
     isDragging=false;
-    img.dataset.offsetX='';
-    img.dataset.offsetY='';
-}
-
+    container.dataset.offsetX='';
+    container.dataset.offsetY='';
+    console.log("stop");
+};
 function applyBoundaryConstraints(x,y){
-    const maxX=window.innerWidth-img.offsetWidth;
-    const maxY=window.innerHeight-img.offsetHeight;
+    const maxX=window.innerWidth-container.offsetWidth;
+    const maxY=window.innerHeight-container.offsetHeight;
     return[
         Math.min(Math.max(0,x),maxX),
         Math.min(Math.max(0,y),maxY)
     ]
-}
+};
+//拖拽结束
 
+//对话开始
+function talk(){
+    let place=document.querySelector(".normalDialog")
+    place.style.display='block';
+//TODO创建对话框，将对话加入+设置时间，消除和更换
+    let randomNum=Math.floor(Math.random()*chat.length);
+    place.innerHTML=`<p style="text-align:center">${chat[randomNum]}</p>`
+}
+//对话结束
 console.log('init');
-initDrag();
+init();
